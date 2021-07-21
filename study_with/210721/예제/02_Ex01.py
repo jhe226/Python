2437ea5194c8b893a2fb3caf5d24e7443aa263a0
jhe226 <79580469+jhe226@users.noreply.github.com>
2021-07-21 12:48:55 +0900

    Add files via upload

---
+++ study_with/210721/예제/02_Ex01.py
@@ -0,0 +1,12 @@
+# p.g 32
+name = 'Alice'
+age = 25
+address = '''우편번호 12345 서울특별시 영등포구 여의도동 서울빌딩 501호'''
+boyfriend = None
+height = 168.5
+
+print(name)
+print(age)
+print(address)
+print(boyfriend)
+print(height)
